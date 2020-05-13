7d7dd5a59062a60fb41fd9236580292bd57da84f
LuisAngelG <luis.gastelu@tecsup.edu.pe>
2020-05-13 11:54:34 -0500

    Initial Commit

---
+++ SoundBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundBoard
+//
+//  Created by mbtec22 on 5/13/20.
+//  Copyright © 2020 mbtec22. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
