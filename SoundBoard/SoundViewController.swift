14b4d5a911a0b350da8dde76aea3d1abe2cd1a25
LuisAngelG <luis.gastelu@tecsup.edu.pe>
2020-05-13 15:33:21 -0500

    first commit

---
+++ SoundBoard/SoundViewController.swift
@@ -0,0 +1,88 @@
+//
+//  SoundViewController.swift
+//  SoundBoard
+//
+//  Created by mbtec22 on 5/13/20.
+//  Copyright © 2020 mbtec22. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class SoundViewController: UIViewController {
+
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    
+    var audioRecorder : AVAudioRecorder?
+    var audioURL : URL?
+    var audioPlayer : AVAudioPlayer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupRecorder()
+        playButton.isEnabled = true
+
+    }
+    
+    func setupRecorder(){
+        
+        do{
+            //creando una sesión de audio
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSession.Category.playAndRecord)
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+            
+            //creando una dirección para el archivo de audio
+            let basePath : String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            let pathComponents = [basePath, "audio.m4a"]
+            audioURL = NSURL.fileURL(withPathComponents: pathComponents)!
+            
+            print("**********************")
+            print(audioURL!)
+            print("**********************")
+            
+            //crear opciones para el grabado de audio
+            var settings: [String: AnyObject] = [:]
+            settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC) as AnyObject?
+            settings[AVSampleRateKey] = 44100.0 as AnyObject?
+            settings[AVNumberOfChannelsKey] = 2 as AnyObject?
+            
+            //crear el objeto de grabacion de audio
+            audioRecorder = try AVAudioRecorder(url: audioURL!, settings: settings)
+            audioRecorder!.prepareToRecord()
+        } catch let error as NSError{
+            print (error)
+        }
+    }
+    
+    @IBAction func recordTapped(_ sender: Any) {
+        if audioRecorder!.isRecording{
+            //Detener la grabacion
+            audioRecorder?.stop()
+            //Cambiar el texto del boton grabar
+            recordButton.setTitle("Record", for: .normal)
+        }else {
+            //empezar a grabar
+            audioRecorder?.record()
+            //cambiar el titulo del boton a detener
+            recordButton.setTitle("Stop", for: .normal)
+        }
+    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOf: audioURL!)
+            audioPlayer!.play()
+        } catch {}
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        
+    }
+    
+}
